ba8483fb9cf1ae9323b2c981e9d619a7fbae5167
Vasily Yakovenko <bitnikvasya@mail.ru>
2019-06-10 11:34:54 +0300

    Статистика

--- js/stat.js
+++ js/stat.js
@@ -15,6 +15,13 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+var renderText = function (ctx, text, x, y) {
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText(text, x, y);
+};
+
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
@@ -27,31 +34,25 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var randomNumber = function (min, max) {
+  return Math.random() * (max - min) + min;
+};
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 4);
+  renderText(ctx, 'Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2);
+  renderText(ctx, 'Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 4);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = '#ff0000';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
-    }
+    ctx.fillStyle = (names[i] === 'Вы') ? '#ff0000' : 'rgba(0, 0, 255, ' + randomNumber(0.3, 1) + ')';
 
     ctx.fillRect(CLOUD_X + GAP * 4 + (BAR_WIDTH + BAR_GAP) * i, (barHeight + GAP * 7 - (MAX_BAR_HEIGHT * times[i]) / maxTime), BAR_WIDTH, (MAX_BAR_HEIGHT * times[i]) / maxTime);
 
-    ctx.fillStyle = '#000';
-
-    ctx.fillText(names[i], CLOUD_X + GAP * 4 + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + GAP * 6 + barHeight + GAP);
+    renderText(ctx, names[i], CLOUD_X + GAP * 4 + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + GAP * 6 + barHeight + GAP);
   }
 };
