3eafd94f2e76c06a022e9b8267d809ddf324b814
Vasily Yakovenko <bitnikvasya@mail.ru>
2019-06-14 15:20:27 +0300

    Учебный проект: нас орда

--- js/setup.js
+++ js/setup.js
@@ -11,18 +11,26 @@ userDialog.classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-var randFirstName = Math.floor(Math.random() * WIZARD_FIRST_NAMES.length);
-var randSecondName = Math.floor(Math.random() * WIZARD_SECOND_NAMES.length);
-var randCoatColor = Math.floor(Math.random() * WIZARD_COAT_COLORS.length);
-var randEyeColor = Math.floor(Math.random() * WIZARD_EYE_COLORS.length);
-
-var wizards = [
-  {
+var generateWizard = function () {
+  var randFirstName = Math.floor(Math.random() * WIZARD_FIRST_NAMES.length);
+  var randSecondName = Math.floor(Math.random() * WIZARD_SECOND_NAMES.length);
+  var randCoatColor = Math.floor(Math.random() * WIZARD_COAT_COLORS.length);
+  var randEyeColor = Math.floor(Math.random() * WIZARD_EYE_COLORS.length);
+  var randWizard = {
     name: WIZARD_FIRST_NAMES[randFirstName] + ' ' + WIZARD_SECOND_NAMES[randSecondName],
     coatColor: WIZARD_COAT_COLORS[randCoatColor],
     eyeColor: WIZARD_EYE_COLORS[randEyeColor]
-  }
-];
+  };
+
+  return randWizard;
+};
+
+var wizards = [];
+var MAX_WIZARDS = 4;
+
+for (var j = 0; j < MAX_WIZARDS; j++) {
+  wizards.push(generateWizard());
+}
 
 var renderWizard = function () {
   var wizardElement = similarWizardTemplate.cloneNode(true);
